f751e329cb67b73a16e17b06fd09bf7ae0cf7f86
mer <iwangyang@qq.com>
2017-09-22 16:50:29 +0800

    ajax jsonp 跨域

--- admin.js
+++ admin.js
@@ -1,14 +1,30 @@
 // let classify_name = '划水'
 // let url = 'http://192.168.1.126:1337/activity/classify_find?classify_name=' + classify_name
-let url = 'http://192.168.1.126:1337/activity/classify'
-$('#find').html(`<script src="${url}" charset="utf-8"></script>`)
+// let url = 'http://192.168.1.126:1337/activity/classify'
+// $('#find').html(`<script src="${url}" charset="utf-8"></script>`)
 
 // 回调函数
-window.ucloud = {
-    find(data) {
-        log('查询:',data)
-    },
-    classify(data) {
-        log('分类:',data)
-    },
+let get = function(url) {
+    return new Promise(function(ok) {
+        $.ajax({
+           url: url,
+           dataType:"jsonp",
+           jsonp:"callback",
+           type:"get",
+           success:function(res){
+               ok(res)
+           }
+       })
+    })
 }
+
+let url = ''
+// 分类
+// url = 'http://192.168.1.126:1337/activity/classify'
+// 查询
+let classify_name = '骑马'
+url = 'http://192.168.1.126:1337/activity/classify_find?classify_name=' + classify_name
+// 获取
+get(url).then(res => {
+    log(res)
+})
