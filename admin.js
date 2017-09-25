162b7c9d14630a1c243250687784cf912abe9fc1
mer <iwangyang@qq.com>
2017-09-25 15:01:04 +0800

    突然好想你

--- admin.js
+++ admin.js
@@ -1,30 +1,44 @@
-// let classify_name = '划水'
-// let url = 'http://192.168.1.126:1337/activity/classify_find?classify_name=' + classify_name
-// let url = 'http://192.168.1.126:1337/activity/classify'
-// $('#find').html(`<script src="${url}" charset="utf-8"></script>`)
-
 // 回调函数
 let get = function(url) {
     return new Promise(function(ok) {
-        $.ajax({
-           url: url,
-           dataType:"jsonp",
-           jsonp:"callback",
-           type:"get",
-           success:function(res){
-               ok(res)
-           }
-       })
+        Sea.Ajax({
+            url: 'http://192.168.1.126:1337/activity/classify',
+            method: "get",
+        }).then(res => {
+            ok(res)
+        })
     })
 }
 
 let url = ''
-// 分类
-// url = 'http://192.168.1.126:1337/activity/classify'
 // 查询
-let classify_name = '骑马'
-url = 'http://192.168.1.126:1337/activity/classify_find?classify_name=' + classify_name
+// url = 'http://192.168.1.126:1337/activity/classify_find?classify_name=骑马'
+// 添加
+// url = 'http://192.168.1.126:1337/activity/classify/add?classify_name=对象'
+// 删除
+// url = 'http://192.168.1.126:1337/activity/classify/delete?classify_name=对象'
 // 获取
-get(url).then(res => {
-    log(res)
-})
+
+let bindEvent = function() {
+    $('#classify').on('click', 'tag', function() {
+        log(this)
+    })
+}
+
+let initClass = function(url) {
+    get(url).then(res => {
+        let data = JSON.parse(res)
+        let html = ''
+        for (let e of data.classify) {
+            html += `<tag data-name="${e}">${e}</tag>`
+        }
+        $('#classify').html(html)
+    })
+}
+let __main = function() {
+    // 分类
+    initClass('http://192.168.1.126:1337/activity/classify')
+    // 事件
+    bindEvent()
+}
+__main()
